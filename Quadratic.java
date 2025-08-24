167ff88de5fbf03a13ae455d3343e9bc5dde24c7
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-08-25 01:22:32 +0530

    Update Quadratic.java
    removed 1 extra line of code and re-edited.

--- Quadratic.java
+++ Quadratic.java
@@ -3,10 +3,9 @@ public class Quadratic {
     // only works when f(x) is of the type x^2 + bx + c //
     double b = Double.parseDouble(args[0]);
     double c = Double.parseDouble(args[1]);
-    double disc = Math.sqrt(b*b - 4.0*c);
-    double x = (-b + disc)/2.0;
-    double y = (-b - disc)/2.0;
-    System.out.println(x);
-    System.out.println(y);
+    double disc = b*b - 4.0*c;
+    double d = Math.sqrt(disc);
+    System.out.println((-b + d)/2.0);
+    System.out.println((-b - d)2.0);
     }
 }
